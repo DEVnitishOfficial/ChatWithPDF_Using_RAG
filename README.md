b5aaf59916eb07663be5f609748dc40c863ed1ad
DEVnitishOfficial <nitish.naroun123@gmail.com>
2026-02-23 22:23:18 +0530

    added vector embedding, api key not working payment required

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # ChatWithPDF_Using_RAG
 
 * Installed qdrant vector db using the docker
-*  
-*  
+* see the docker file that how we installed it.
+* always run you docker engine first to use it.
+* if you qdrant is up and running check it on localhost:6333 that must appear.
+
+* -qU flag meaning:
+    - q : Quiet(Minimizes log output to keep the console clean)
+    - U : Upgrade(Ensures you are installing the newest available version)
 
 - Use Poetry to manage dependencies, lock versions and create reproducible installs:
 
@@ -16,7 +21,7 @@ poetry run python index.py
 Installation
 Install langchain-community and pypdf.
 pip install -qU langchain-community pypdf 
-pip freeze > requirement.txt
+pip freeze > requirements.txt
 
 
 docs : https://docs.langchain.com/oss/python/integrations/document_loaders/pypdfloader
@@ -29,3 +34,21 @@ pip install -U langchain-text-splitters
 pip freeze > requirement.txt
 
 docs : https://docs.langchain.com/oss/python/integrations/splitters
+
+## vector embedding
+
+Installation
+The LangChain OpenAI integration lives in the langchain-openai package:
+pip install -qU langchain-openai
+
+docs: https://docs.langchain.com/oss/python/integrations/text_embedding/openai
+
+
+## install langchain_qdrant using which we can talk to our qdrant db(it's a bridge b/w langchain and qdrant vector db)
+
+Qdrant integration
+Integrate with the Qdrant vector store using LangChain Python.
+pip install -qU langchain-qdrant
+
+docs: https://docs.langchain.com/oss/python/integrations/vectorstores/qdrant
+
--- index.py
+++ index.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
+
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_qdrant import QdrantVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv() # load environment variables from .env file
 
 pdf_path = Path(__file__).parent / "Nodejs_pdf.pdf"
 
@@ -13,11 +19,23 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 
 chunks = text_splitter.split_documents(docs)
 print(f"Total chunks: {len(chunks)}")
-# print the first chunk
-# print(f"First chunk: {chunks[0].page_content}")
-# print(f"First chunk metadata: {chunks[0].metadata}")
-# print(f"second chunk: {chunks[1].page_content}")
 
-print(f"fifty chunk: {chunks[50].page_content}")
-print(f"fiftyone chunk: {chunks[51].page_content}")
+# create vector embeddings for the chunks
 
+# Here embedding model will take those created chunks and then it will store
+# in the qdrant database(vector db) and then we can query that database to get 
+# the relevant chunks based on the query
+embeddings_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+# install langchain_qdrant package to use Qdrant vector database(works 
+# like bridge between langchain and qdrant)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embeddings_model,
+    url="http://localhost:6333", # open you localhost:6333/dashboard to see the qdrant dashboard
+    collection_name="my_first_rag_collection" # name of the collection in qdrant database where the chunks will be stored,
+)
+
+print("Vector store created and chunks stored in Qdrant database successfully!")
+print("Indexing completed.")
